82600b057ac3cf9a425f4140ded9d376eb7ebbe0
Aryan Singh <lucky.aryan115@gmail.com>
2026-04-27 17:04:50 +0530

    Initial commit

---
+++ CICDDemoApp/CICDDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDDemoAppApp.swift
+//  CICDDemoApp
+//
+//  Created by Aryan Singh on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CICDDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CICDDemoApp
+//
+//  Created by Aryan Singh on 27/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
