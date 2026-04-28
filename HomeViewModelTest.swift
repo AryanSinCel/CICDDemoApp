cef8c1032c5e30313017fa6b0a2a3fa2ad958f14
Aryan Singh <lucky.aryan115@gmail.com>
2026-04-28 10:07:16 +0530

    Refactor iOS CI pipeline to improve test execution and streamline build settings, including adjustments to Xcode version handling.

---
+++ HomeViewModelTest.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModelTest.swift
+//  CICDDemoApp
+//
+//  Created by Aryan Singh on 28/04/26.
+//
+
+import Testing
+
+struct HomeViewModelTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
